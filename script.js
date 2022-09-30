cccadf6332dc7f7668acbcfe7461f1a6961fe285
Yash <yash111ams@gmail.com>
2022-09-30 15:37:52 +0530

    JS and Css file completed

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-textInput = document.querySelector(".textInput")
-btn = document.querySelector(".btn")
-textResult = document.querySelector(".textResult")
+let textInput = document.querySelector(".textInput");
+let btn = document.querySelector(".btn");
+let textResult = document.querySelector(".textResult");
 
+function check() {
+  let removeChar = textInput.value.replace(/[^A-Z0-9]/gi, "").toLowerCase();
+
+  let checkPalindrome = removeChar.split("").reverse().join("");
+
+  /* Check to see if str is a Palindrome*/
+  if (removeChar === checkPalindrome) {
+    textResult.innerHTML = textInput.value + " is a Palindrome";
+  } else {
+    textResult.innerHTML = textInput.value + " is not a Palindrome";
+  }
+}
